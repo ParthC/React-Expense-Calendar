ef6e1f6b82b1cf0493bc7ac62c2aaca4b0a1b5f1
Parth Chauhan <parthchauhan@Vaibhavs-MacBook-Pro-15.local>
2022-10-15 14:54:47 -0400

    int project

---
+++ src/components/Card.js
@@ -0,0 +1,12 @@
+import '../css/card.css'
+
+const Card = (props) => {
+    const accessChildClass = 'card ' + props.className
+  return (
+    <div className={accessChildClass}>{props.children}</div>
+  ) 
+}
+
+export default Card
+
+
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import ExpenseDate from './ExpenseDate'
+import Card from './Card'
+import '../css/expenseItem.css'
+
+
+const ExpenseItem = (props) => {
+
+  const [title, setTitle] = useState(props.title)
+
+  const onClickButton = () => {
+    setTitle('test pass')
+  }
+
+  return (
+    <Card className='expense-item'>
+      <ExpenseDate date={props.date}/>
+      <div className='expense-item__description'>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>${props.amount} </div>
+      </div>
+      <button onClick={onClickButton}>click me !!</button>
+    </Card>
+  )
+}
+
+export default ExpenseItem
+ 
