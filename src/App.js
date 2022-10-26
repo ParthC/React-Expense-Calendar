07ddd420c2bd4f72fcfdfe1409a69e7502a28d82
Parth Chauhan <parthchauhan2702@gmail.com>
2022-10-25 22:46:37 -0400

    Added chart content

--- src/App.js
+++ src/App.js
@@ -1,37 +1,43 @@
+import  {useState} from 'react'
 import Expenses from './components/Expense/Expenses';
 import UserExpense from './components/NewExpense/UserExpense';
 
+const mockData = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 9, 12) },
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: 'e4',
+    title: 'New Desk (Wooden)',
+    amount: 450,
+    date: new Date(2021, 4, 12),
+  },
+];
+
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+
+  const [mockDataExpense, setMockData] = useState(mockData)
 
   const addExpenseHandler = (expenses) => {
-    console.log(expenses)
+    setMockData((previousMockData) => {
+      return [expenses, ...previousMockData]
+    })
   }
 
   return (
     <div>
       <UserExpense onAddExpense={addExpenseHandler}/>
-      <Expenses items={expenses} />
+      <Expenses items={mockDataExpense} />
     </div>
   );
 }
--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -1,32 +1,11 @@
 import React, { useState } from 'react';
 import '../../css/expense.css';
-import ExpenseItem from './ExpenseItem';
+import ExpenseList from './ExpenseList';
 import ExpensesFilter from '../Expense/ExpenseFilter';
+import ExpenseChart from './ExpensesChart'
 import Card from '../UI/Card'
 
 const Expenses = (props) => {
-  // const expenses = [
-  //   {
-  //     id: 'e1',
-  //     title: 'Toilet Paper',
-  //     amount: 94.12,
-  //     date: new Date(2020, 7, 14),
-  //   },
-  //   { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-  //   {
-  //     id: 'e3',
-  //     title: 'Car Insurance',
-  //     amount: 294.67,
-  //     date: new Date(2021, 2, 28),
-  //   },
-  //   { 
-  //     id: 'e4',
-  //     title: 'New Desk',
-  //     amount: 450,
-  //     date: new Date(2021, 5, 12),
-  //   },
-  // ];
-
   const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
@@ -38,17 +17,17 @@ const Expenses = (props) => {
   });
 
   return (
-    <Card className='expenses'>
+    <div>
+      <Card className='expenses'>
         <h2 className='text'> Calendar </h2>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem title={props.items[0].title} amount={props.items[0].amount} date={props.items[0].date}/>
-        <ExpenseItem title={props.items[1].title} amount={props.items[1].amount} date={props.items[1].date}/>
-        <ExpenseItem title={props.items[2].title} amount={props.items[2].amount} date={props.items[2].date}/>
-        <ExpenseItem title={props.items[3].title} amount={props.items[3].amount} date={props.items[3].date}/>
-    </Card>
+        <ExpenseChart expense={filteredExpenses} />
+        <ExpenseList items={filteredExpenses}/>
+      </Card>
+    </div>
   );
 }
 
